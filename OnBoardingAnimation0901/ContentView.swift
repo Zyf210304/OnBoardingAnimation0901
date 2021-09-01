cb74f6f3cdccbccde36821150165916b77f5023e
张亚飞 <672233580@qq.com>
2021-09-01 13:11:11 +0800

    Initial Commit

---
+++ OnBoardingAnimation0901/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnBoardingAnimation0901
+//
+//  Created by 张亚飞 on 2021/9/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnBoardingAnimation0901/OnBoardingAnimation0901App.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingAnimation0901App.swift
+//  OnBoardingAnimation0901
+//
+//  Created by 张亚飞 on 2021/9/1.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingAnimation0901App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
